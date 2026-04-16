8584ab2b70146cd4d9a4ef9a20a46a3b745c6cb1
elazarweiss <elazarweiss@gmail.com>
2026-04-16 16:34:15 +0300

    chore: auto-launch scanner when app starts

--- run_growbook.bat
+++ run_growbook.bat
@@ -1,5 +1,9 @@
 @echo off
 set PATH=C:\flutter\bin;%PATH%
 cd /d "%~dp0"
+
+:: Start scanner in a minimized background window
+start /min "GrowBook Scanner" "%~dp0run_scanner.bat"
+
 flutter run -d chrome
 pause
